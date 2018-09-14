7cb307bb8ab41f21116e69773392a890a45510fc
Dan Kempton <kemptondan@gmail.com>
2018-09-14 15:35:57 -0600

    added js output

--- js/app.js
+++ js/app.js
@@ -8,5 +8,25 @@ $(document).ready(() => {
 })
 
 function getMovies(searchText) {
-  console.log(searchText);
+  // Making request to the API using Axios
+  axios.get('http://www.omdbapi.com?s='+searchText+'&apikey=thewdb') // concatenating searchText to return it
+  .then((response) => {
+    console.log(response);
+    let movies = response.data.Search; // this puts the movies searched array into this variable
+    let output = '';
+    movies.forEach(movie = (index) => {
+      output += `
+        <div class="col-md-3">
+          <div class="well text-center">
+            <img src="${movie.Poster}"/>
+            <h5>${movie.Title}</h5>
+            <a onClick="movieSelected('${movie.imdbID}')"></a> 
+          </div>
+        </div>
+      `
+    })
+  } )
+  .catch((err) => {
+    console.log(err);
+  });
 }
